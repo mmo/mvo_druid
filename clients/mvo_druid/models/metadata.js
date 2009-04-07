2548fe1a8fd13bc8d4d892c270c0194ab6e7659b
mom <miguel.moreira@rero.ch>
2009-04-07 17:55:59 +0200

    Completed demo for e-lib.ch meeting in Bern, 27.03.2009

---
+++ clients/mvo_druid/models/metadata.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// MvoDruid.Metadata
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document your class here)
+
+  @extends SC.Record
+  @author AuthorName
+  @version 0.1
+*/
+MvoDruid.Metadata = SC.Record.extend(
+/** @scope MvoDruid.Metadata.prototype */ {
+
+	properties: [
+		'type',
+		'author',
+		'title'
+	]
+
+});
